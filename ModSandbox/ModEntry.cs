3070b50c70e7cbb0235b6d4b93e28defd9d816dd
Andy <a97pearce@gmail.com>
2024-03-26 13:39:52 +0000

    Refactor and attempt to scale with UI - needs rethink

--- ModSandbox/ModEntry.cs
+++ ModSandbox/ModEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
@@ -20,102 +21,50 @@ namespace ModSandbox
         *********/
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
-		private int currentStamina;
-		private int currentHealth;
-		private int tickCount = 0;
-		private int staminaIconX = 1;
-		private int staminaIconY = 412;
-		private int statusIconWxH = 14;
-		private int healthIconX = 17;
-		private int healthIconY = 412;
-		private int healthBarOffset = 50;
+		
 		private Texture2D tileSheet;
-		private Texture2D staminaIcon;
-		private Vector2 staminaTextPosition;
-		private Vector2 healthTextPosition;
-		private Rectangle sourceStaminaRect;
-		private Rectangle destStaminaRect;
-		private Rectangle sourceHealthRect;
-		private Rectangle destHealthRect;
 		private string cursorsTileSheetPath = "\\Content\\LooseSprites\\cursors.xnb";
-		private float scaleFactor = 2.0f;
+		private Farmer player;
+		private UpdateHud updateHud;
+		private Logger logger;
+		//private float previousUiScale;
+		//private RenderingHudEventArgs renderingHudEventArgs;
 
 		public override void Entry(IModHelper helper)
 		{
 			tileSheet = helper.ModContent.Load<Texture2D>(Path.Combine(Constants.GamePath + cursorsTileSheetPath));
 
+			renderingHudEventArgs = new RenderingHudEventArgs();
+
 			// Subscribe to the game's ticks
 			helper.Events.Display.RenderingHud += OnRenderingHud;
+			//helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
 		}
 
         private void OnRenderingHud(object sender, RenderingHudEventArgs e)
 		{
-			Farmer player = Game1.player;
+			// Initialize things
+			renderingHudEventArgs = e;
+			player = Game1.player;
+			updateHud = new UpdateHud();
+			logger = new Logger();
 
-			UpdateHudStatusText(player, e);
+			updateHud.UpdateHudStatusText(player, tileSheet, e);
 
-			TickLogger();
-		}        
-
-		private void UpdateHudStatusText(Farmer player, RenderingHudEventArgs e)
-		{
-			// Reload viewport
-			int statusDestX = Game1.viewport.Width - 210;
-			int staminaDestY = Game1.viewport.Height - 50;
-			int healthDestY = Game1.viewport.Height - 80;
-			int statusTextX = Game1.viewport.Width - 170;
-
-			// Get stamina and health values
-			currentHealth = player.health;
-			currentStamina = (int)player.stamina;
-
-			// Create the source rectangles for cropping
-			sourceStaminaRect = new Rectangle(staminaIconX, staminaIconY, statusIconWxH, statusIconWxH);
-			sourceHealthRect = new Rectangle(healthIconX, healthIconY, statusIconWxH, statusIconWxH);
-
-			// Calculate the destination rectangles for rendering
-			int destWxH = (int)(statusIconWxH * scaleFactor);
-
-			// Calculate position for image based on the text position and current game location
-			switch (Game1.currentLocation)
-			{
-				case MineShaft _:
-				case Woods _:
-				case SlimeHutch _:
-				case VolcanoDungeon _:
-					destStaminaRect = new Rectangle(statusDestX, staminaDestY, destWxH, destWxH);
-					destHealthRect = new Rectangle(statusDestX, healthDestY, destWxH, destWxH);
-					staminaTextPosition = new Vector2(statusTextX, staminaDestY);
-					healthTextPosition = new Vector2(statusTextX, healthDestY);
-					break;
-				default:
-					destStaminaRect = new Rectangle(statusDestX + healthBarOffset, staminaDestY, destWxH, destWxH);
-					destHealthRect = new Rectangle(statusDestX + healthBarOffset, healthDestY, destWxH, destWxH);
-					staminaTextPosition = new Vector2(statusTextX + healthBarOffset, staminaDestY);
-					healthTextPosition = new Vector2(statusTextX + healthBarOffset, healthDestY);
-				break;
-			}			
-
-			// Draw value and icon to the screen
-			SpriteBatch spriteBatch = e.SpriteBatch;
-			spriteBatch.DrawString(Game1.smallFont, $"{currentStamina}", staminaTextPosition, Color.White);
-			spriteBatch.Draw(tileSheet, destStaminaRect, sourceStaminaRect, Color.White);
-			spriteBatch.DrawString(Game1.smallFont, $"{currentHealth}", healthTextPosition, Color.White);
-			spriteBatch.Draw(tileSheet, destHealthRect, sourceHealthRect, Color.White);
+			logger.TickLogger();
 		}
 
-		private void TickLogger()
-		{
-			// Only display debug info every few seconds
-			tickCount++;
-			if (tickCount == 180)
-			{
-				Game1.showingHealth = !Game1.showingHealth;
-				// DEBUG
-				Monitor.Log($"CurrentStamina = {currentStamina}", LogLevel.Debug);
-				//Monitor.Log($"x pos: {textPosition.X} | y pos: {textPosition.Y}", LogLevel.Debug);
-				tickCount = 0;
-			}
-		}
+		//private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
+		//{
+		//	if (!Context.IsWorldReady)
+		//		return;
+
+		//	float currentUiScale = Game1.options.uiScale;
+
+		//	if (currentUiScale != previousUiScale)
+		//	{
+		//		OnRenderingHud(sender, renderingHudEventArgs);
+		//	}
+		//}
     }
 }
---
+++ ModSandbox/UpdateHud.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI.Events;
+using StardewValley.Locations;
+using StardewValley;
+
+namespace ModSandbox
+{
+	public class UpdateHud
+	{
+		private int currentStamina;
+	 	private int currentHealth;
+		private int staminaIconX = 1;
+		private int staminaIconY = 412;
+		private int healthIconX = 17;
+		private int healthIconY = 412;
+		private int statusIconWxH = 14;
+		private int healthBarOffset = 50;
+		private int viewportMarginRightIcons = 200;
+		private int viewportMarginRightText = 170;
+		private int viewportMarginBottomStamina = 50;
+		private int viewportMarginBottomHealth = 80;
+		private Vector2 staminaTextPosition;
+		private Vector2 healthTextPosition;
+		private Rectangle sourceStaminaRect;
+		private Rectangle destStaminaRect;
+		private Rectangle sourceHealthRect;
+		private Rectangle destHealthRect;
+		private float scaleFactor = 2.0f;
+
+		public void UpdateHudStatusText(Farmer player, Texture2D tileSheet, RenderingHudEventArgs e)
+		{
+			// Reload viewport
+			int statusDestX = Game1.viewport.Width - viewportMarginRightIcons;
+			int staminaDestY = Game1.viewport.Height - viewportMarginBottomStamina;
+			int healthDestY = Game1.viewport.Height - viewportMarginBottomHealth;
+			int statusTextX = Game1.viewport.Width - viewportMarginRightText;
+
+			// Get stamina and health values
+			currentHealth = player.health;
+			currentStamina = (int)player.stamina;
+
+			// Create the source rectangles for cropping
+			sourceStaminaRect = new Rectangle(staminaIconX, staminaIconY, statusIconWxH, statusIconWxH);
+			sourceHealthRect = new Rectangle(healthIconX, healthIconY, statusIconWxH, statusIconWxH);
+
+			// Scale up the icons
+			int destWxH = (int)(statusIconWxH * scaleFactor);
+
+			// Calculate position for image based on the text position and current game location
+			switch (Game1.currentLocation)
+			{
+				case MineShaft _:
+				case Woods _:
+				case SlimeHutch _:
+				case VolcanoDungeon _:
+				case Farm _:
+					destStaminaRect = new Rectangle(statusDestX, staminaDestY, destWxH, destWxH);
+					destHealthRect = new Rectangle(statusDestX, healthDestY, destWxH, destWxH);
+					staminaTextPosition = new Vector2(statusTextX, staminaDestY);
+					healthTextPosition = new Vector2(statusTextX, healthDestY);
+				break;
+				default:
+					destStaminaRect = new Rectangle(statusDestX + healthBarOffset, staminaDestY, destWxH, destWxH);
+					destHealthRect = new Rectangle(statusDestX + healthBarOffset, healthDestY, destWxH, destWxH);
+					staminaTextPosition = new Vector2(statusTextX + healthBarOffset, staminaDestY);
+					healthTextPosition = new Vector2(statusTextX + healthBarOffset, healthDestY);
+				break;
+			}
+
+			// Draw value and icon to the screen
+			SpriteBatch spriteBatch = e.SpriteBatch;
+			spriteBatch.DrawString(Game1.smallFont, $"{currentStamina}", staminaTextPosition, Color.White);
+			spriteBatch.Draw(tileSheet, destStaminaRect, sourceStaminaRect, Color.White);
+			spriteBatch.DrawString(Game1.smallFont, $"{currentHealth}", healthTextPosition, Color.White);
+			spriteBatch.Draw(tileSheet, destHealthRect, sourceHealthRect, Color.White);
+		}
+	}
+}
